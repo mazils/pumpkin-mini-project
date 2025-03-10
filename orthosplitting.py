d7b7c0c32a130c74885691388e1006cc428c4f0b
Tobi Adetula <oluwatobi.adetula1@gmail.com>
2025-03-10 23:18:50 +0100

    Refactor OrthoSplitter to improve file handling and add field cropping and splitting functionality

--- orthosplitting.py
+++ orthosplitting.py
@@ -10,7 +10,6 @@ script_dir = os.path.dirname(__file__)
 
 input_rel = "../figures"
 ortho_rel = "../visualized-qgis-images"
-ortho_test_rel = "../test_orthophotos"
 output_rel = "../processed-images"
 
 
@@ -19,8 +18,7 @@ output_path = os.path.join(script_dir, output_rel)
 ortho_path = os.path.join(script_dir, ortho_rel)
 
 ortho_file = ortho_path + "/Gyldensteensvej-9-19-2017-orthophoto.tif"
-ortho_test = ortho_test_rel + "/Hans-Christian-Andersen-Airport-10-24-2018-orthophoto.tif"
-output_file = ortho_path + "/output.tif"
+output_file = output_path + "/output.tif"
 
 class OrthoSplitter:
     def __init__(self, ortho_file, output_file):
@@ -32,39 +30,82 @@ class OrthoSplitter:
         self.rows = None
         self.bands = None
         self.window_location = None
+        self.field = None
         self.img = None
         self.ortho_meta = None
 
     def load_ortho(self):
-        with rasterio.open(self.ortho_file) as src:
-            self.ortho = src.read()
-            self.ortho_meta = src.meta
-            self.columns = src.width
-            self.rows = src.height
-            self.bands = src.count   
-            # print(ortho_meta) # print metadata
-            print(self.ortho.shape)
-            print(self.columns, self.rows, self.bands)
-            # Define the tile coordinates (upper left corner and lower right corner)
-            tile_ulc = (2310, 1474)  # Upper left corner (row, col)
-            tile_lrc = (5813, 11487)  # Lower right corner (row, col)
+        self.dataset = rasterio.open(self.ortho_file)
+        self.ortho = self.dataset.read()
+        self.ortho_meta = self.dataset.meta
+        self.columns = self.dataset.width
+        self.rows = self.dataset.height
+        self.bands = self.dataset.count   
+        print("Ortho loaded")    
+        print(f"Initial Values = Columns: {self.columns}, Rows: {self.rows}, Bands: {self.bands}")
+        
+        return self.ortho, self.ortho_meta, self.columns, self.rows, self.bands
+    
             
-            # Create a window using the tile coordinates
-            # window_location = Window.from_slices((tile_ulc[0], tile_lrc[0]), (tile_ulc[1], tile_lrc[1]))    
-            self.window_location = Window.from_slices(slice(1, -1), slice(1, -1), height=5000, width=10000)
-            
-            self.img = src.read(window=self.window_location)
-            print(self.img.shape)
-            # Update metadata to reflect the new dimensions
-            self.ortho_meta.update({
-                "height": self.window_location.height,
-                "width": self.window_location.width,
-                "transform": rasterio.windows.transform(self.window_location, src.transform)
-            })
+    def crop_field(self):
+        """
+        Crop the orthophoto to the field of interest
+        """
+        # Define the tile coordinates (upper left corner and lower right corner)
+        tile_ulc = (2310, 1000)  # Upper left corner (row, col)
+        tile_lrc = (5810, 12000)  # Lower right corner (row, col)
+        
+        # Create a window using the tile coordinates
+        self.window_location =  Window.from_slices((tile_ulc[0], tile_lrc[0]), (tile_ulc[1], tile_lrc[1]))    
+        
+        self.field = self.dataset.read(window=self.window_location)
+        print(self.field.shape)
+        # Update metadata to reflect the new dimensions
+        self.ortho_meta.update({
+            "height": self.window_location.height,
+            "width": self.window_location.width,
+            "transform": rasterio.windows.transform(self.window_location, self.dataset.transform)
+        })
+        
+        return self.field
+    
+    
+    def split_field(self, field = None, slices = 4):
+        """
+        Split the field into smaller tiles
+        """
+        if field is None:   
+            field = self.field
+        # Current Shape of the field
+        bands, rows, cols = field.shape 
+        
+        # Calculate the number of rows and columns for each tile
+        row_slices = rows // slices
+        col_slices = cols // slices
+        print(row_slices, col_slices)
 
+        for i in range(slices):
+            for j in range(slices):
+                # Calculate the window coordinates for each tile
+                window = Window.from_slices((i * row_slices, (i + 1) * row_slices), (j * col_slices, (j + 1) * col_slices))
+                print(window)
+                # Read the tile
+                tile = field[:, window.row_off:window.row_off + window.height, window.col_off:window.col_off + window.width]
+                print(tile.shape)
+                # Write the tile to the output directory
+                with rasterio.open(os.path.join(output_path, f"output_tile_{i}_{j}.tif"), 'w', **self.ortho_meta) as dst:
+                    dst.write(tile)
+                print(f"Tile {i}_{j} written to output file")
+
+        
+        
+    def write_ortho(self, img=None):
+        if img is None:
+            img = self.field
             # Write the image to the output directory
             with rasterio.open(self.output_file, 'w', **self.ortho_meta) as dst:
-                dst.write(self.img)
+                dst.write(img)
+            print("Ortho written to output file")
 
 
 
@@ -72,51 +113,11 @@ class OrthoSplitter:
 def main():
     ortho_splitter = OrthoSplitter(ortho_file, output_file)
     ortho_splitter.load_ortho()
-    # with rasterio.open(ortho_file) as src:
-    #     ortho = src.read()
-    #     ortho_meta = src.meta
-    #     columns = src.width
-
-with rasterio.open(ortho_file) as src:
-    ortho = src.read()
-    ortho_meta = src.meta
-    columns = src.width
-    rows = src.height
-    bands = src.count   
-    # print(ortho_meta) # print metadata
-    print(ortho.shape)
-    print(columns, rows, bands)
-    # Define the tile coordinates (upper left corner and lower right corner)
-    tile_ulc = (2310, 1474)  # Upper left corner (row, col)
-    tile_lrc = (5813, 11487)  # Lower right corner (row, col)
-    
-    # Create a window using the tile coordinates
-    # window_location = Window.from_slices((tile_ulc[0], tile_lrc[0]), (tile_ulc[1], tile_lrc[1]))    
-    window_location = Window.from_slices(slice(1, -1), slice(1, -1), height=5000, width=10000)
-    
-    img = src.read(window=window_location)
-    print(img.shape)
-    # Update metadata to reflect the new dimensions
-    ortho_meta.update({
-        "height": window_location.height,
-        "width": window_location.width,
-        "transform": rasterio.windows.transform(window_location, src.transform)
-    })
-
-    # Write the image to the output directory
-    with rasterio.open(output_file, 'w', **ortho_meta) as dst:
-        dst.write(img)
-    # temp = img.transpose(1, 2, 0)
-    # t2 = cv.split(temp)
-    # img_cv = cv.merge([t2[2], t2[1], t2[0]])
-    # cv.imshow("test", img_cv)
-    # cv.waitKey(0)
-                                  
-                      
-                   
+    ortho_splitter.crop_field()
+    ortho_splitter.split_field()
+    ortho_splitter.write_ortho()
+    print("Ortho split and written to output file")
 
 
-# with rasterio.open(   output_file, 'w',
-#         driver='GTiff', width=500, height=300, count=1,
-#         dtype=image.dtype) as dst:
-#     dst.write(img, window=Window(50, 30, 250, 150), indexes=1)
+if __name__ == "__main__":
+    main()
