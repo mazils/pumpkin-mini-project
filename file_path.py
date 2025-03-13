2d74e221cb34bb90a14f0005be3b15edbe15015d
Tobi Adetula <oluwatobi.adetula1@gmail.com>
2025-03-13 12:55:25 +0100

    Add file_path.py to define paths for input and output images

---
+++ file_path.py
@@ -0,0 +1,19 @@
+import os
+
+input_rel = "../figures"
+
+script_dir = os.path.dirname(__file__)
+
+input_rel = "../figures"
+ortho_rel = "../visualized-qgis-images"
+annotated_rel = "../annotated-images"  
+output_rel = "../processed-images"
+
+
+input_path = os.path.join(script_dir, input_rel)
+output_path = os.path.join(script_dir, output_rel)  
+ortho_path = os.path.join(script_dir, ortho_rel)
+annotated_path = os.path.join(script_dir, annotated_rel)
+
+ortho_file = ortho_path + "/Gyldensteensvej-9-19-2017-orthophoto.tif"
+output_file = output_path + "/field.tif"
